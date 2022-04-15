c4e3181d581c000f406da8ca8d77b902ed3157d6
Jovi <766255988@qq.com>
2022-04-16 01:03:42 +0800

    更新

--- src/main/java/cn/vonce/sql/android/helper/SqlBeanHelper.java
+++ src/main/java/cn/vonce/sql/android/helper/SqlBeanHelper.java
@@ -4,7 +4,7 @@ package cn.vonce.sql.android.helper;
 import cn.vonce.sql.android.service.SqlBeanServiceImpl;
 
 /**
- * 生成表字段常量处理器
+ * SqlBean数据库助手
  *
  * @author Jovi
  * @version 1.0
@@ -12,7 +12,6 @@ import cn.vonce.sql.android.service.SqlBeanServiceImpl;
  * @date 2022/4/16 0:35
  */
 public class SqlBeanHelper<T, ID> extends SqlBeanServiceImpl<T, ID> {
-
 
     public SqlBeanHelper(Class<?> clazz, DatabaseHelper databaseHelper) {
         super(clazz, databaseHelper);
